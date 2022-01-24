eb89894d9a4ee0ddd3ad0e5261f0b432bbc9e995
PriyaCoder <88959450+Priyacoder1@users.noreply.github.com>
2022-01-24 16:35:32 +0530

    Add files via upload

---
+++ misc.js
@@ -0,0 +1,75 @@
+function getMouseVector(){
+	let mouseXalt = mouseX - turPosX;
+	let mouseYalt = mouseY - turPosY;
+	let mouseDir = createVector(mouseXalt, mouseYalt);
+	mouseDir.normalize();
+	return mouseDir;
+}
+	
+function drawReticle(){
+	noFill();
+	strokeWeight(1.5);
+	stroke(0, 100, 125, 125);
+	ellipse(mouseX, mouseY, 20);
+	stroke(80, 160, 200, 125);
+	line(mouseX-14, mouseY-14, mouseX+14, mouseY+14);
+	line(mouseX+14, mouseY-14, mouseX-14, mouseY+14);
+	stroke(80, 160, 200, 125);
+	line(turPosX, turPosY, mouseX, mouseY);
+}
+
+function gameOver(){
+	push()
+	
+	print("DED");
+	noStroke();
+	fill(20)
+	rect(0,200,600,200)
+	
+	textFont('Georgia');
+	textAlign(CENTER);
+	textSize(50);
+	fill(255,69,0);
+	text("YOU LOSS!",300,200)
+ 	text("TRY AGAIN!",300,500)
+		
+	textFont('Helvetica');
+	textSize(18);
+	fill(235);
+	let scoreString = "score: " + score;
+	text(scoreString, 300, 340);
+	
+	if (score > HighScore) {
+		HighScore = score;
+		Cookies.remove('HighScore');
+		Cookies.set('HighScore', HighScore);
+	}
+	
+	let HighScoreString = "HighScore: " + HighScore;
+	text(HighScoreString, 300, 360);
+	
+	Retry.show();
+	Retry.position(250, 380);
+	Retry.size(100,30);
+	Retry.style('background-color', '#202020');
+	Retry.style('color', '#FFFFFF');
+	Retry.mousePressed(reset);
+	
+	pop();
+	noLoop();
+	
+}
+
+function reset(){
+	Retry.hide();
+	bulletsFired = [];
+	targetBalloons = [];
+	turPosX = 300;
+	turPosY = 300;
+	targetTimer = 0;
+	balloonSpawnMultiplier = 2;
+	balloonSizeMultiplier = 2;
+	score = 0;
+	
+	loop();
+}
